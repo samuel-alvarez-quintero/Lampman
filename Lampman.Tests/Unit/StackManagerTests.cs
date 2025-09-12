72676153a66008df36f828635e6f39876e0b159b
Samuel Alvarez Quintero <42385434+samuel-alvarez-quintero@users.noreply.github.com>
2025-09-12 14:05:25 -0300

    test: add integration and unit tests for StackCommand and StackManager functionality

---
+++ Lampman.Tests/Unit/StackManagerTests.cs
@@ -0,0 +1,79 @@
+using DotNetEnv;
+using Lampman.Core;
+using Lampman.Core.Services;
+using Lampman.Tests.Fixtures;
+using Lampman.Tests.TestHelpers;
+
+namespace Lampman.Tests.Unit;
+
+[Trait("Category", "Unit"), Trait("Category", "StackManager"), TestCaseOrderer(typeof(PriorityOrderer))]
+public class StackManagerTests : IClassFixture<MockRegistryFixture>
+{
+    private readonly StackManager _stackManager;
+
+    private readonly string[]? servicesToManage;
+
+    private readonly MockRegistryFixture _fixture;
+
+    public StackManagerTests(MockRegistryFixture fixture)
+    {
+        Env.TraversePath().Load();
+
+        _fixture = fixture;
+
+        _stackManager = new StackManager();
+
+        string? services = Environment.GetEnvironmentVariable("TESTING_SERVICES_TO_MANAGE");
+
+        if (!string.IsNullOrEmpty(services))
+        {
+            servicesToManage = services.Split(';', StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+
+    /** Test the 'lampman list' commands via StackManager class **/
+    [Fact, Trait("Category", "Manager_StackList"), TestPriority(200)]
+    public void StackList_ShouldDisplayServicesListing()
+    {
+        _stackManager.ListServices();
+    }
+
+    /** Test the 'lampman start [service-key]' commands via StackManager class **/
+    [Fact, Trait("Category", "Manager_StackStart"), TestPriority(201)]
+    public void StackStart_ShouldRunServiceInstalled()
+    {
+        if (null != servicesToManage && servicesToManage.Length > 0)
+        {
+            foreach (string service in servicesToManage)
+            {
+                _stackManager.StartServices([service]);
+            }
+        }
+    }
+
+    /** Test the 'lampman restart [service-key]' commands via StackManager class **/
+    [Fact, Trait("Category", "Manager_StackRestart"), TestPriority(202)]
+    public void StackRestart_ShouldRestartServiceInstalled()
+    {
+        if (null != servicesToManage && servicesToManage.Length > 0)
+        {
+            foreach (string service in servicesToManage)
+            {
+                _stackManager.RestartServices([service]);
+            }
+        }
+    }
+
+    /** Test the 'lampman stop [service-key]' commands via StackManager class **/
+    [Fact, Trait("Category", "Manager_StackStop"), TestPriority(203)]
+    public void StackStop_ShouldStopServiceInstalled()
+    {
+        if (null != servicesToManage && servicesToManage.Length > 0)
+        {
+            foreach (string service in servicesToManage)
+            {
+                _stackManager.StartServices([service]);
+            }
+        }
+    }
+}
